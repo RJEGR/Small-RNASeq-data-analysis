8a94f1f38e6a52b6127f3687e9264bc2d10629a3
Ricardo Gore <rjhgore@gmail.com>
2023-03-09 20:08:05 -0800

    Update PROFILING_LIBS_BY_READ_LENGTH.R

--- DOWNSTREAM/DIFFEXP/PROFILING_LIBS_BY_READ_LENGTH.R
+++ DOWNSTREAM/DIFFEXP/PROFILING_LIBS_BY_READ_LENGTH.R
@@ -60,12 +60,12 @@ read_df_and_summarise <- function(x) {
 
 # out <- do.call(rbind, out)
 
-write_rds(out, file = paste0(path, "prof_by_read_length_summary.rds"))
+# write_rds(out, file = paste0(path, "prof_by_read_length_summary.rds"))
 
-out <- read_rds(paste0(path, "prof_by_read_length_summary.rds")) %>% left_join(mtd)
+out <- read_rds(paste0(path, "prof_by_read_length_summary.rds")) %>% left_join(mtd) %>%
+  mutate(first_nuc = recode(first_nuc, `T` = "U"))
 
-# Identification of known and novel miRNAs: 
-#The first base preference of known miRNA mature. 18~30-nt sRNAs were selected for analyzed and each histogram indicated the percentage of first base in the sRNAs with same RNA number ... Besides, we analyzed the first base preference for known mature miRNA (Fig. 2). Among these four groups: Ch-3, Ch-5, sample group A and sample group B showed preference towards U for the first base in 18 ~ 23-nt sRNAs, but the first base preference in 24 ~ 30-nt sRNAs was different between the four groups. sampleC and sampleD had more U preference than sampleA and samplegroupB in 24 ~ 35-nt sRNA
+# 1) -----
 
 xlab <- "Read Length (nt)"
 ylab <- "Percentage of reads"
@@ -75,17 +75,72 @@ out %>%
   group_by(group, Length, rnatype) %>% summarise(n = sum(n)) %>%
   group_by(group, Length) %>% mutate(pct = n / sum(n)) %>%
   ggplot(aes(x = Length, y = pct, fill = rnatype)) + 
-  facet_grid(group ~ .) + geom_col() +
+  facet_grid(group ~ ., scales = "free_y") + geom_col(width = 0.85) +
   scale_y_continuous(ylab, labels = scales::percent) +
+  # scale_y_continuous(ylab, labels = scales::comma) + # if y = n
   scale_x_continuous(xlab, breaks = seq(min(out$Length),max(out$Length), by = 1)) +
-  see::scale_fill_pizza() +
-  theme_bw(base_family = "GillSans") -> ps
+  see::scale_fill_pizza() -> ps
 
-ggsave(ps, filename = 'PROFILING_SAMPLES_BY_READ_LENGTH.png', path = path, width = 12, height = 6.5)
+ps <- ps + theme_bw(base_family = "GillSans", base_size = 11) +
+  theme(strip.background = element_rect(fill = 'white', color = 'white'),
+    legend.position = 'top',
+    panel.border = element_blank(),
+    panel.grid.minor = element_blank())
+    # axis.text.y = element_blank(), 
+    # axis.ticks.y = element_blank(),
+    # panel.grid.major = element_blank()) 
+
+ggsave(ps, filename = 'PROFILING_SAMPLES_BY_READ_LENGTH.png', path = path, width = 6, height = 5, device = png)
 
 # out %>% ggplot(aes(x = Length, y = n, fill = rnatype)) + geom_col()
 
-# BY PCT
+# 2) BY PCT ----
 
-df %>% ggplot(aes(x = Length, y = pct, fill = first_nuc)) + geom_col() + 
-  facet_wrap(~ rnatype, scales = 'free_y', nrow = 5)
+# The first base preference of known miRNA mature. 18~30-nt sRNAs were selected for analyzed and each histogram indicated the percentage of first base in the sRNAs with same RNA number ... Besides, we analyzed the first base preference for known mature miRNA (Fig. 2). Among these four groups: Ch-3, Ch-5, sample group A and sample group B showed preference towards U for the first base in 18~23-nt sRNAs, but the first base preference in 24~30-nt sRNAs was different between the four groups. sampleC and sampleD had more U preference than sampleA and samplegroupB in 24~35-nt sRNA
+
+
+xlab <- "Read Length (nt)"
+ylab <- "Percentage of reads"
+
+out %>% 
+  # group_by(Length, rnatype, first_nuc) %>% summarise(n = sum(n)) %>%
+  # group_by(Length, rnatype) %>% mutate(pct = n / sum(n)) %>%
+  ggplot(aes(x = Length, y = n, fill = first_nuc)) + 
+  facet_grid( rnatype ~., scales = 'free_y') + geom_col(width = 0.85) +
+  # scale_y_continuous(ylab, labels = scales::percent) +
+  scale_y_continuous(ylab, labels = scales::comma) +
+  scale_x_continuous(xlab, breaks = seq(min(out$Length),max(out$Length), by = 1)) +
+  see::scale_fill_social(reverse = T) -> ps
+
+ps <- ps + theme_bw(base_family = "GillSans", base_size = 11) +
+  theme(strip.background = element_rect(fill = 'white', color = 'white'),
+    legend.position = 'top',
+    panel.border = element_blank(),
+    panel.grid.minor = element_blank())
+
+ggsave(ps, filename = 'PROFILING_SAMPLES_BY_READ_LENGTH_NUC.png', path = path, width = 6, height = 5, device = png)
+
+
+# 3) miRNA 1st position ----
+
+ylab <- "Number of reads"
+breaks_seq <- seq(18, 25, by = 1)
+
+out %>% 
+  filter(rnatype %in% 'miRNAs') %>%
+  mutate(group = paste0(hpf, "+", pH)) %>% # group = sample_id
+  group_by(Length, group, first_nuc) %>% summarise(n = sum(n)) %>%
+  group_by(Length, group) %>% mutate(pct = n / sum(n)) %>%
+  ggplot(aes(x = Length, y = n, fill = first_nuc)) + 
+  facet_grid( group ~., scales = 'free_y') + geom_col(width = 0.85) +
+  # scale_y_continuous(ylab, labels = scales::percent) +
+  scale_y_continuous(ylab, labels = scales::comma) +
+  scale_x_continuous(xlab, breaks = breaks_seq, limits = c(17, 26)) +
+  see::scale_fill_social(reverse = T) +
+  theme_bw(base_family = "GillSans", base_size = 11) +
+  theme(strip.background = element_rect(fill = 'white', color = 'white'),
+    legend.position = 'top',
+    panel.border = element_blank(),
+    panel.grid.minor = element_blank()) -> ps
+
+ggsave(ps, filename = 'PROFILING_SAMPLES_BY_READ_LENGTH_NUC_kmiRNAs.png', path = path, width = 4, height = 4.5, device = png)
