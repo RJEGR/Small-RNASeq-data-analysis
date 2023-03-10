9e35971ea2a9c20ca74101d8e6d6a46c2e8ecbe7
Ricardo Gore <rjhgore@gmail.com>
2023-03-09 20:08:09 -0800

    Update SETTINGS.R

--- DOWNSTREAM/DIFFEXP/SETTINGS.R
+++ DOWNSTREAM/DIFFEXP/SETTINGS.R
@@ -1,11 +1,11 @@
 
 
 # change graphical device/driver (istead of "RStudioGD")
-options("device")
-dev.set(4)
+# options("device")
+# dev.set(4)
 # INCLUDE FONTS
 # instead if ggthemes
 # install.packages('extrafont')
 # library(extrafont)
-font_import()
-extrafont::loadfonts(device = "all", quiet = TRUE)
+# font_import()
+# extrafont::loadfonts(device = "all", quiet = TRUE)
