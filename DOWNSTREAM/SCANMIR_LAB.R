40cb74160af9649d9f2fe4be88740b0ee87218fe
Ricardo Gore <rjhgore@gmail.com>
2023-04-11 22:13:42 -0700

    Update SCANMIR_LAB.R

--- DOWNSTREAM/SCANMIR_LAB.R
+++ DOWNSTREAM/SCANMIR_LAB.R
@@ -19,18 +19,19 @@ library(tidyverse)
 # 0) Input queries ----
 # Using a miRNA sequences predicted by shortstacks
 
-path <- "~/Documents/MIRNA_HALIOTIS/SHORTSTACKS/ShortStack_20230315_out//"
+wd <- "~/Documents/MIRNA_HALIOTIS/"
 
+query_path <- paste0(wd, "/SHORTSTACKS/ShortStack_20230315_out/")
+ref_path <- paste0(wd, "/RNA_SEQ_ANALYSIS/ASSEMBLY/STRINGTIE/REFBASED_MODE/ANNOTATION/")
 
-res_f <- list.files(path = path, pattern = "Results.txt", full.names = T)
-
+res_f <- list.files(path = query_path, pattern = "Results.txt", full.names = T)
 
 RESULTS <- read_tsv(res_f)
 
 RESULTS %>% dplyr::count(MIRNA)
 
-
 #  pull(MajorRNA, name = Name) 
+
 MajorRNA <- RESULTS %>% filter(MIRNA == "Y") %>% pull(MajorRNA) 
 
 MajorRNA <- sample(MajorRNA, 10)
@@ -44,28 +45,91 @@ MajorRNA <- sample(MajorRNA, 10)
 
 # 1.2) Genomic five_prime_utr and three_prime_utr flanked fasta format
 
-f <- '/Users/cigom/Documents/MIRNA_HALIOTIS/ENSEMBLE/utr_rmdup.fa'
+# f <- '/Users/cigom/Documents/MIRNA_HALIOTIS/ENSEMBLE/utr_rmdup.fa'
+
+# `seqkit rmdup -s -P -D duplicated_detail.txt -d duplicates.fasta -o utr_rmdup.fa`
+
+# cat transcripts.fa.transdecoder.cds | seqkit rmdup -s -P -D duplicated_detail.txt -d transcripts.fa.transdecoder.dups.cds -o transcripts.fa.transdecoder.rmdup.cds
+
+
+# 108812 CDS
+# 108812 exon
+# 87680 five_prime_UTR
+# 108812 gene
+# 108812 mRNA
+# 100219 three_prime_UTR
+
+
+gff_f <- list.files(path = ref_path, pattern = "transcripts.fa.transdecoder.gff3", full.names = T)
+
+# nrow(ape::read.gff(gff_f))
+
+gff <- read_tsv(gff_f, comment = "#", col_names = F)
+
+gffNames <- c("seqname", "source", "feature", "start", "end", "score", "strand", "frame", "attributes")
+
+names(gff) <- gffNames
+
+seqname_id <- gff %>% 
+  filter(grepl("prime_UTR",feature)) %>% 
+  arrange(seqname) %>% distinct(seqname) %>% pull()
+
+f <- list.files(path = ref_path, pattern = "transcripts.fa.transdecoder.rmdup.cds", full.names = T)
+
 
 seqs <- Biostrings::readDNAStringSet(f)
+
+str(keep <- names(seqs))
+
+keep <- sapply(strsplit(keep, " "), `[`, 1)
+
+str(keep <- unique(keep))
+
+sum(keep <- keep %in% seqname_id)
 
 subseqs <- sample(seqs, 10)
 
 # 2) run scan ----
 # matches2 <- findSeedMatches(seqs, SampleKdModel, verbose = FALSE)
 
-matchesdf <- findSeedMatches(subseqs, MajorRNA, verbose = FALSE, keepMatchSeq = T)
+matchesdf <- findSeedMatches(subseqs, MajorRNA, verbose = F, keepMatchSeq = T)
 
-head(matchesdf)
+
+MajorRNA <- gsub("U", "T", MajorRNA)
+
+hist(matchesdf$p3.score)
+
+table(matchesdf$type)
+
+n <- 2
+
+sum(keep <- as.character(matchesdf$miRNA) %in% MajorRNA[n])
+
+m <- matchesdf[keep,]
+
+m 
+
+seqnames <- subseqs[names(subseqs) %in% as.character(m@seqnames)[5]]
+
+viewTargetAlignment(m[5,], MajorRNA[n], seqs = seqnames)
+
+?scanMiR::plotKdModel()
+
+matchesdf
+
+matchesdf %>% view()
 
 matchesdf <- as.data.frame(matchesdf)
 
-mrna_id <- unique(matchesdf$seqnames)
+mrna_id <- as.character(matchesdf$seqnames)
+
+# mrna_id <- sapply(strsplit(mrna_id, " "), `[`, 1)
 
 mrna_seq <- seqs[names(seqs) %in% mrna_id]
 mrna_seq <- as.character(mrna_seq)
 names(mrna_seq) <- NULL
 
-mirna_seq <- matchesdf$miRNA
+mirna_seq <- as.character(matchesdf$miRNA)
 
 label <- matchesdf$p3.score
 # mirna_id <- ...
