a2a7094b1e474edaf097371c313343e3d00a3da0
Ricardo Gore <rjhgore@gmail.com>
2023-06-23 16:16:10 -0700

    Create MICRORNA_CONSERVATION_EXPLORATORY.R

---
+++ DOWNSTREAM/DIFFEXP/MICRORNA_CONSERVATION_EXPLORATORY.R
@@ -0,0 +1,13 @@
+
+# MICRORNA CONSERVATION
+
+# /Users/cigom/Documents/MIRNA_HALIOTIS/MIRGENEDB_20230314
+# /Users/cigom/Documents/MIRNA_HALIOTIS/PIRNA_DB
+
+
+# SEARCH START NUCLEOTIDE PREFERENCY IN PIRS AND MIRS (FROM SHORTSTACKS)
+
+
+# SEARCH MOTIF CONSERVATION USING MEME OR OTHER R PACKAGE FOR MIRS
+#  https://omarwagih.github.io/ggseqlogo/#installation
+devtools::install_github("omarwagih/ggseqlogo")
