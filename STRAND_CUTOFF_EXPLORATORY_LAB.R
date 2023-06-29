b82a927c3e3d0ac61854511e197a9729d65f696b
Ricardo Gore <rjhgore@gmail.com>
2023-06-29 13:38:06 -0700

    Update STRAND_CUTOFF_EXPLORATORY_LAB.R

--- STRAND_CUTOFF_EXPLORATORY_LAB.R
+++ STRAND_CUTOFF_EXPLORATORY_LAB.R
@@ -7,3 +7,14 @@ DB %>%
   geom_histogram() + facet_wrap(~ Strand, scales = "free_y")
 
 # default: 0.8. Loci with >80% reads on the top genomic strand are '+' stranded, loci with <20% reads on the top genomic strand are '-' stranded, and all others are unstranded '.'
+
+wd <- "/Users/cigom/Documents/MIRNA_HALIOTIS/SHORTSTACKS/ShortStack_20230315_out"
+
+head(DB <- read_tsv(paste0(wd, "/RNA_LOCATION_DB.tsv")))
+
+
+# ANALYSIS OF CANONICAL OR ISOFORM (NEILSEN ET AL 2012)
+DB %>% 
+  drop_na(KnownRNAs) %>%
+  ggplot(aes(MajorRNAReads/Reads)) +
+  geom_histogram()
