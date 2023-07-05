9b8209325c97ce39f55767ca9d3f28f4d1089f38
Ricardo Gore <rjhgore@gmail.com>
2023-07-04 17:24:45 -0700

    Update 2_SRNA_LOCATION_DB_PREP.R

--- 2_SRNA_LOCATION_DB_PREP.R
+++ 2_SRNA_LOCATION_DB_PREP.R
@@ -15,14 +15,107 @@ library(GenomicRanges)
 
 wd <- "/Users/cigom/Documents/MIRNA_HALIOTIS/SHORTSTACKS/ShortStack_20230315_out"
 
-head(.out <- read_rds(paste0(wd, "/SRNA_LOCATION_OUT.rds")))
+out <- read_rds(paste0(wd, "/SRNA_LOCATION_OUT.rds"))
 
-.out <- do.call(rbind, .out) %>% as_tibble()
+print(.out <- do.call(rbind, out) %>% as_tibble())
 
-# .out %>% count(type)
+# 1) HIERARCHICAL CATEGORIES FOR THE GENOMIC ANNOTATIONS ==== 
 
-# .out %>% count(Name)
+# SIMILAR TO ISAAC MARTINEZ UGALDE (CEI ABREU STUDENT)
+# GENOMIC ANNOTATION MUST BE CURATED BASED ON 
+# OVERLAPPING FEATURES ARE HIERARCHICALY CATEGORIZED FOR EACH GENOMIC REGION.
+# THEN, ARE USED TO IDENTIFY MIRNA/SRNAS SOURCE
+# 1) USING SETDIFF (FROM GENOMICRANGES; Lawrence et al., 2013) TO OBTAIN THE NON-INTERSECTED REGION AND THE INTERSECTED
+# 1.2) GENOMIC CATEGORIES ARE SUMMARIZED IN biotypeL OBJECT
+# 1.3) THE LEVEL OF SRNA PRODUCTION IN EACH GENOMIC REGION WAS OBTAINED USING subsetByOverlaps
 
+
+# 1.1) CURATE NA biotype ====
+# CURATE NA biotype based on type 
+
+.out %>% filter(is.na(biotype)) %>% count(type, sort = T)
+
+.out <- .out %>% 
+  mutate(biotype = ifelse(type == "exon", type, biotype)) %>% 
+  mutate(biotype = ifelse(type == "CDS", type, biotype)) %>% 
+  mutate(biotype = ifelse(grepl("prime_UTR", type), "UTR", biotype))
+
+# Sanity check
+.out %>% filter(is.na(biotype)) %>% count(type, sort = T) # MUST BE EMPTY
+
+
+# 1.2) MASK INTRAGENIC ====
+# (DOI: 10.1111/mec.14973) miRNA intronic (intragenic) locations were those that intersected with a gene but were not annotated as either an exon or an UTR 
+  
+.out %>% count(biotype, sort = T) 
+
+.out %>% filter(biotype == "protein_coding") %>% count(type, sort = T)
+.out %>% filter(biotype == "misc_RNA") %>% count(type, sort = T)
+.out %>% filter(biotype == "pseudogene") %>% count(type, sort = T)
+
+recode_to <- c(`protein_coding` = "Intragenic", `misc_RNA`= "Intragenic",`pseudogene` = "Intragenic", `mRNA` = "Intragenic", `gene` = "Intragenic")
+
+.out <- .out %>% 
+  # mutate(SRNA_CLASS = biotype) %>%
+  dplyr::mutate(biotype = dplyr::recode_factor(biotype, !!!recode_to)) %>%
+  dplyr::mutate(type = dplyr::recode_factor(type, !!!recode_to))
+
+known_repeat_ranks <- c(
+  "Simple_repeat","LTR/ERV1", "LTR/Gypsy", "DNA/TcMar-Tc2", "Low_complexity",
+  "LINE/CR1", "DNA/TcMar-Mariner", "DNA/hAT-Charlie", "Satellite", 
+  "LTR/ERVL-MaLR", "DNA/TcMar-Tigger", "DNA/MULE-MuDR", "Unknown", 
+  "Satellite/centr", "LINE/RTE-X", "SINE/MIR", "LINE/L2", "LINE/L1") 
+
+recode_to <- structure(rep("Repeat element", 
+  length(known_repeat_ranks)), names =known_repeat_ranks)
+
+.out <- .out %>% 
+  # mutate(SRNA_CLASS = biotype) %>%
+  dplyr::mutate(biotype = dplyr::recode_factor(biotype, !!!recode_to))
+
+
+.out %>% count(biotype, sort = T) 
+
+# WHAT ABOUT NON CODING BIOTYPES??
+# ARE ALSO INTRAGENIC? MAY BE TRUE!
+# OR BETTER, REMOVE FROM DOWNSTREAM ANALYSIS!
+
+.out %>% filter(biotype == "rRNA") %>% count(type, sort = T)
+.out %>% filter(biotype == "lncRNA") %>% count(type, sort = T)
+.out %>% filter(biotype == "tRNA") %>% count(type, sort = T)
+
+.out %>% filter(biotype == "Simple_repeat") %>% count(type, sort = T)
+.out %>% filter(biotype == "srpRNA") %>% count(type, sort = T)
+.out %>% filter(biotype == "snoRNA") %>% count(type, sort = T)
+.out %>% filter(biotype == "snRNA") %>% count(type, sort = T)
+.out %>% filter(biotype == "Unknown") %>% count(type, sort = T)
+
+# 1.3) PREP RANK BIOTYPE HIERARCHIC CATEGORIZE ====
+
+# (doi.org/10.1101/2022.12.30.522274) Non redundant (nr) genomic annotation mask may be build to reduce the Intra-range features (i.e overlapped annotations) and assign mapped sequences to unique/single annotations. Using preference (or hierarchical) annotation selection as follow: rRNA > tRNA, Transposable elements (TE) > protein-coding exon, other ncRNAs, introns, pseudogenes ... 
+
+
+known_non_coding_ranks <- c("tRNA", "rRNA","lncRNA", "snoRNA", "srpRNA", "snRNA")
+
+primary_ranks <- c("Repeat element", "UTR","exon", "CDS")
+
+last_ranks <- c("Intergenic", "Intragenic")
+
+biotypeL <- c(primary_ranks, known_non_coding_ranks, last_ranks)
+
+.out <- .out %>% mutate(biotype = factor(biotype, levels = biotypeL))
+
+# sanity check
+
+identical(levels(.out$biotype), biotypeL) # MUST BE TRUE
+
+pick_rank <- function(x) {
+  
+  x <- unique(x[!is.na(x)])
+  x <- levels(x)[sort(as.integer(x))][1]
+}
+
+# USE THEN IN STEP 4)
 
 paste_headers <- function(x) { 
   x <- x[!is.na(x)] 
@@ -32,11 +125,6 @@ paste_headers <- function(x) {
   x <- paste(x, sep = '|', collapse = '|') 
   x <- paste(n, x, sep = '|', collapse = '|') }
 
-# out <- .out %>%
-#   head(100) %>%
-#   group_by(Name, biotype, gene_id) %>%
-#   summarise(across(type, .fns = paste_headers), .groups = "drop_last") %>% 
-#   ungroup()
 
 # out %>% drop_na(gene_id)
 
@@ -93,10 +181,6 @@ DB <- DB %>% left_join(.DB, by = "Name")
 
 # 4) BIND LOCATION DB ====
 
-# HAY QUE REVISAR QUE EFECTO TIENE STRAND EN EL ANALISIS DE OVERLAPS, 
-# PUES NO COINCIDEN LAS COORDENADAS GENOMICAS (.out) CON LAS DE LA BASE DE DATOS (DB)
-
-# DB %>% drop_na(KnownRNAs) %>% count(Locus_type, Strand, sort = T) %>% view()
 
 bind_types <- function(x) { x <- paste(unique(x[!is.na(x)]), sep = '|', collapse = "|") }
 
@@ -107,16 +191,17 @@ bind_ids <- function(x) {
   x <- unique(x)
   x <- paste(x, sep = ';', collapse = ';') }
 
-
 .DB <- .out %>%
   group_by(Name) %>%
+  mutate(biotype_best_rank = biotype) %>%
   summarise(
+    across(biotype_best_rank, .fns = pick_rank),
     across(type, .fns = bind_types), 
     across(biotype, .fns = bind_types),
     across(gene_id, .fns = bind_ids),
     across(transcript_id, .fns = bind_ids),
     .groups = "drop_last") %>% 
-  ungroup() 
+  ungroup()
 
 DB <- DB %>% left_join(.DB, by = "Name")
 
@@ -138,7 +223,7 @@ any(names(bwmodules) %in% DB$Name)
 
 DB$WGCNA  <- bwmodules[match(DB$Name, names(bwmodules))]
 
-DB %>% head() %>% view()
+# DB %>% head() %>% view()
 
 write_tsv(DB, file = paste0(wd, "/RNA_LOCATION_DB.tsv"))
 
