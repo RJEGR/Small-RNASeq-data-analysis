586d3322c053a314b6f28a3945b0e0d732cbf8a1
Ricardo Gore <rjhgore@gmail.com>
2023-07-13 09:36:53 -0700

    Update TARGETSCAN_EXPLORATORY.R

--- DOWNSTREAM/DIFFEXP/TARGETSCAN_EXPLORATORY.R
+++ DOWNSTREAM/DIFFEXP/TARGETSCAN_EXPLORATORY.R
@@ -1,4 +1,5 @@
 # TARGETSCAN
+# ESTE ANALISIS ES DIRIGIDO A SOLO A LOS 131 MIRS:
 
 rm(list = ls())
 
@@ -12,13 +13,22 @@ f <- "mature_star_mir_vs_mir_vs_utr_rmdup_RNAhybrid.out.psig_targetscan.out"
 
 f <- list.files(path = wd, pattern = f, full.names = T)
 
+utr_f <- list.files(path = wd, full.names = T, pattern = "three_prime_utr.ids")
+
+str(x <- read_lines(utr_f)) # 54 432 <-- i.e the N sequences from three_prime_utr.fa
+
+
+str(three_prime_utr <- sapply(strsplit(x, " "), `[`, 1)) # three_prime_utr ids 
+
 library(tidyverse)
 
 df <- read_tsv(f)
 
 df %>% distinct(a_Gene_ID) # 861
 
-df %>% distinct(miRNA_family_ID) # 262
+df %>% distinct(miRNA_family_ID) # 262 mir:mir* 
+
+# Q: how many arm-binding sites found?
 
 df %>%
   mutate(MIRNA = "hairpin") %>%
@@ -26,6 +36,88 @@ df %>%
   mutate(MIRNA = ifelse(grepl("star", miRNA_family_ID), "star", MIRNA)) %>%
   count(MIRNA)
 
-# OVERLAP WITH RNAHYBRID
+# 
+
+df <- df %>% 
+  separate(a_Gene_ID, into = c("target","gene_id"), sep = ";") %>%
+  separate(miRNA_family_ID, into = c("query", "coor"), sep = "::") 
+
+# 
+df <- df %>% 
+  mutate(UTR = ifelse(target %in% three_prime_utr, "three_prime_utr", "five_prime_utr"))
+
+df %>% count(UTR)
+
+TARGETSCAN <- df %>%
+  group_by(query) %>%
+  count(sort = T)
+
+df1 <- read_rds(paste0(wd, "/mir_vs_utr_rmdup_RNAhybrid.out.psig.gene_features.rds"))
+
+RNAHYBRID <- df1 %>%
+  mutate(query = strsplit(query, ";")) %>%
+  unnest(query) %>%
+  # filter(target %in% three_prime_utr) %>%
+  group_by(query) %>%
+  count(sort = T) 
+
+RNAHYBRID %>% 
+  left_join(TARGETSCAN, by = "query") %>% 
+  rename("RNAHYBRID"="n.x", "TARGETSCAN"="n.y") %>%
+  separate(query, into = c("Name", "arm"), sep = "[.]") %>%
+  pivot_longer(cols = c("RNAHYBRID", "TARGETSCAN"), names_to = "Tool") %>%
+  ggplot(aes(value, fill = Tool, color = Tool)) + 
+  geom_histogram() +
+  theme_bw(base_size = 12, base_family = "GillSans") +
+  scale_x_continuous("Number of target", labels = scales::comma) +
+  scale_y_continuous("Number of miRs", labels = scales::comma) +
+  scale_color_manual(values = c("#00AFBB", "#E7B800")) +
+  scale_fill_manual(values = c("#00AFBB", "#E7B800")) +
+  theme(strip.background = element_rect(fill = 'white', color = 'white'),
+    legend.position = 'right',
+    panel.border = element_blank(),
+    panel.grid.minor = element_blank()) 
+
+# SEARCH CONSISTENCY BETWEEN RNAhybrid AND TARGETSCAN
+
+df1 <- df1 %>%
+  mutate(query = strsplit(query, ";")) %>%
+  unnest(query) %>% 
+  filter(target %in% three_prime_utr)
+
+str(q.RNAhybrid <- df1 %>% mutate(ID = paste(target, query, sep = "|")) %>% distinct(ID) %>% pull())
+str(q.targetscan <- df %>% mutate(ID = paste(target, query, sep = "|")) %>% distinct(ID) %>% pull())
 
 
+q.RNAhybrid[q.RNAhybrid %in% q.targetscan]
+sum(q.RNAhybrid %in% q.targetscan)
+
+nrow(df) # 13 418
+
+nrow(df1) # 18 050
+
+nrow(.DB <- df1 %>% inner_join(df, by = c("target", "query")))
+
+view(.DB %>% drop_na(gene_id))
+
+# OVERLAP w/ DB
+
+wd <- "/Users/cigom/Documents/MIRNA_HALIOTIS/SHORTSTACKS/ShortStack_20230315_out"
+
+DB <- read_tsv(paste0(wd, "/RNA_LOCATION_DB.tsv"))
+
+# 
+
+DB %>% left_join(.DB)
+
+
+# HEATMAP FROM ABUND.
+df1 <- df1 %>% separate(query, into = c("Name", "arm"), sep = "[.]")
+
+df1 <- df1 %>% drop_na(description) 
+
+df1 <- df1 %>% distinct(gene_id, description, Name)
+
+COUNTS %>% right_join(df1)
+
+df1 %>% arrange(desc(description))
