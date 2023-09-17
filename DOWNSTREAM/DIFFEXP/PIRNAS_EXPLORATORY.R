d3b22a71c2a1486c2f115fb3674c6076960982e9
Ricardo Gore <rjhgore@gmail.com>
2023-09-17 11:16:15 -0700

    move scripts

--- DOWNSTREAM/DIFFEXP/PIRNAS_EXPLORATORY.R
+++
@@ -1,7 +0,0 @@
-# Considerations and complications of mapping small RNA high-throughput data to transposable elements (read paper: Alexandros Bousios1* , Brandon S. Gaut2 and Nikos Darzentas3)
-
-# The challenges of mapping sRNAs to TEs are exacerbated by the fact that accurate TE identification is a notoriously difficult task.
-
-# TE-derived dsRNA can lead to gene or TE silencing or to activation of the interferon response (Lanciano S. 2017 Measuring and interpreting transposable element expression)
-
-# PIWI genes and piRNAs are ubiquitously expressed in mollusks and show patterns of lineage-specific adaptation (DOI: 10.1038/s42003-018-0141-4)
