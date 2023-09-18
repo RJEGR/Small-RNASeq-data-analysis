7120012dbb5ce8b15523e7d8e479ecee1e86253a
Ricardo Gore <rjhgore@gmail.com>
2023-09-17 22:47:24 -0700

    Update 3_DESEQ2TOPGO.R

--- E_expression_analysis/3_DESEQ2TOPGO.R
+++ E_expression_analysis/3_DESEQ2TOPGO.R
@@ -31,9 +31,9 @@ EXCLUSIVE_MIRS <- .UPSETDF[[1]] %>% ungroup()
 
 INTERSECTED_MIRS <- .UPSETDF[[2]]  %>% ungroup()
 
-EXCLUSIVE_MIRS %>% count(CONTRAST, SIGN, sort = T) # ???
+EXCLUSIVE_MIRS %>% dplyr::count(CONTRAST, SIGN, sort = T) 
 
-INTERSECTED_MIRS %>% count(CONTRAST, SIGN, sort = T)
+INTERSECTED_MIRS %>% dplyr::count(CONTRAST, SIGN, sort = T)
 
 wd <- "/Users/cigom/Documents/MIRNA_HALIOTIS/FUNCTIONAL_MIR_ANNOT/"
 
@@ -94,6 +94,113 @@ SRNA2GO <- split(strsplit(SRNA2GO$GO.ID, ";") , SRNA2GO$query)
 
 SRNA2GO <- lapply(SRNA2GO, unlist)
 
+# INTERSECTED ====
+
+# 38+46=84 mirs shared under low and control pH at 24 HPF and 110 HPF, respectively
+
+INTERSECTED_MIRS %>% dplyr::count(CONTRAST, SIGN, sort = T)
+
+INTERSECTED_MIRS %>% distinct(Name)
+  
+str(WHICH_SIGN <- INTERSECTED_MIRS %>% ungroup() %>% distinct(SIGN) %>% pull())
+
+DF <- list()
+
+for(j in 1:length(WHICH_SIGN)) {
+  
+  i <- j
+  
+  cat("\nRunning ",WHICH_SIGN[i], "\n")
+  
+  query.names <- INTERSECTED_MIRS %>% ungroup() %>% filter(SIGN %in% WHICH_SIGN[i]) %>% distinct(Name) %>% pull(Name)
+  
+  query.p <- c(rep(0.05, length(query.names)))
+  
+  allRes <- GOenrichment(query.p, query.names, SRNA2GO, Nodes = 20)
+  
+  allRes <- allRes %>% mutate(SIGN = WHICH_SIGN[i]) %>% as_tibble()
+  
+  DF[[i]] <- allRes
+  
+}
+
+INTERSECTED_MIRS_ALLRes <- do.call(rbind, DF) %>% mutate(CONTRAST = "Both pH")
+
+view(INTERSECTED_MIRS_ALLRes)
+
+
+INTERSECTED_MIRS %>% distinct(Name) %>%
+  left_join(RES.P) %>% group_by(Name) %>% view()
+
+
+INTERSECTED_MIRS_ALLRes %>%
+  group_by(SIGN) %>% arrange(Term) %>%
+  # mutate(Term = factor(Term, levels= unique(Term))) %>%
+  mutate(Term = fct_reorder(Term, Annotated)) %>%
+  ggplot(aes(y = Term, x = Annotated, fill = p.adj.ks)) + # , fill = p.adj.ks fill = -log10(p.adj.ks))
+  geom_col() +
+  # geom_text(aes(label = Annotated), color = 'black', family = "GillSans", size = 3, angle = 0, hjust = -0.5) +
+  theme_bw(base_family = "GillSans", base_size = 12) +
+  ggh4x::facet_nested( SIGN~., nest_line = F, scales = "free", space = "free") +
+  theme_bw(base_family = "GillSans", base_size = 12) +
+  ggsci::scale_fill_material("blue-grey") +
+  labs(x = "miRs anotados", y = "") +
+  theme(strip.background = element_rect(fill = 'grey89', color = 'white'),
+    # legend.position = "top",
+    panel.border = element_blank(),
+    plot.title = element_text(hjust = 0),
+    plot.caption = element_text(hjust = 0),
+    panel.grid = element_blank(),
+    # axis.text.y.right = element_text(angle = 0, hjust = 1, vjust = 0, size = 2.5),
+    axis.text.y = element_text(angle = 0, size = 7),
+    axis.text.x = element_text(angle = 0, size = 10)) -> p
+
+ggsave(p, filename = 'DESEQ2TOPGO_INTERSECTED.png', path = wd, width = 7, height = 4, device = png, dpi = 300)
+
+# INTERSECTED_MIRS_ALLRes
+# SEMANTIC ====
+
+library(GOSemSim)
+
+# hsGO <- godata('org.Hs.eg.db', ont="BP")
+
+# rbind(TOPGO_INTERSECTED, TOPGO_EXCLUSIVE)
+
+
+str(GO.ID <- INTERSECTED_MIRS_ALLRes %>% distinct(GO.ID) %>% pull())
+
+# hsGO <- GOSemSim::godata('org.Hs.eg.db', ont="BP")
+
+hsGO <- read_rds(paste0(wd, '/hsGO_BP.rds'))
+
+termSim <- GOSemSim::termSim(GO.ID, GO.ID,  semData = hsGO, method = "Wang")
+
+# heatmap(termSim)
+
+cmds <- termSim %>% 
+  # use distance metric
+  dist(method = "euclidean") %>%
+  # compute cMDS
+  cmdscale() %>%
+  data.frame() %>%
+  rownames_to_column(var= 'GO.ID')
+
+cutree <- termSim %>% 
+  # use distance metric
+  dist(method = "euclidean") %>%
+  # compute cMDS
+  hclust() %>%
+  cutree(., 7)
+
+# Sanity check
+
+identical(cmds$GO.ID, names(cutree))
+
+head(cmds <- cbind(cmds, cutree) %>% mutate(cutree = as.factor(cutree)))
+
+INTERSECTED_MIRS_ALLRes <- INTERSECTED_MIRS_ALLRes %>% left_join(cmds)
+
+
 # EXCLUSIVE ====
 
 # CONTRAST SIGN           n
@@ -105,10 +212,20 @@ WHICH_SIGN <- "A) 24 HPF"
 
 str(CONTRAST <- EXCLUSIVE_MIRS %>% ungroup() %>% filter(SIGN == WHICH_SIGN) %>% distinct(CONTRAST) %>% pull())
 
-EXCLUSIVE_MIRS %>% unnest(CONTRAST_DE) %>% filter(SIGN == WHICH_SIGN) %>% count(SIGN, CONTRAST)
+CONTRAST <- as.character(CONTRAST)
 
-str(query.names <- EXCLUSIVE_MIRS %>% distinct(Name) %>% pull(Name))
+EXCLUSIVE_MIRS %>% unnest(CONTRAST_DE) %>% filter(SIGN == WHICH_SIGN) %>% dplyr::count(SIGN, CONTRAST)
 
+str(query.names <- EXCLUSIVE_MIRS %>% unnest(CONTRAST_DE) %>% 
+    filter(SIGN == WHICH_SIGN) %>% distinct(Name) %>% pull())
+
+query.p <- c(rep(0.05, length(query.names)))
+
+EXCLUSIVE_MIRS_ALLRes_24HPF <- GOenrichment(query.p, query.names, SRNA2GO, Nodes = 3)
+
+EXCLUSIVE_MIRS_ALLRes_24HPF <- EXCLUSIVE_MIRS_ALLRes_24HPF %>% mutate(SIGN = WHICH_SIGN, CONTRAST = CONTRAST) %>% as_tibble()
+
+view(EXCLUSIVE_MIRS_ALLRes_24HPF)
 
 # RES.P %>%
 #   filter(Name %in% query.names) %>%
@@ -133,48 +250,31 @@ drop.names <- EXCLUSIVE_MIRS %>%
   filter(n == 1) %>%
   unnest(Name) %>% 
   pull(Name)
-  
 
-# UP
 
-DF <- list()
+WHICH_SIGN <- "B) 110 HPF"
 
-for(j in 1:length(CONTRAST)) {
-  
-  i <- j
-  
-  cat("\nRunning ",CONTRAST[i], "\n")
-  
-  # DF2GO <- RES %>% filter(CONTRAST %in% CONTRAST[i])
-  
-  # DF2GO <- DF2GO %>% filter(padj < 0.05) # filter(log2FoldChange < 0 )
-  
-  # query.p <- DF2GO %>% pull(padj)
-  
-  # query.names <- DF2GO %>% pull(Name)
-  
-  query.names <- EXCLUSIVE_MIRS %>% ungroup() %>% 
-    filter(SIGN == WHICH_SIGN) %>% 
-    filter(CONTRAST %in% CONTRAST[i]) %>% distinct() %>% pull(Name)
-  
-  # RES.P %>% filter(Name %in% query.names) %>% filter(CONTRAST %in% CONTRAST[i]) %>% view()
-  
-  
-  query.p <- c(rep(0.05, length(query.names)))
-  
-  cat("\nUsing ",length(query.names), " Names...\n")
-  
-  cat("\nAnd ",sum(names(SRNA2GO) %in% query.names), " GO terms\n")
-  
-  allRes <- GOenrichment(query.p, query.names, SRNA2GO, Nodes = 5)
-  
-  allRes <- allRes %>% mutate(CONTRAST = CONTRAST[i]) %>% as_tibble()
-  
-  DF[[i]] <- allRes
-  
-}
+str(CONTRAST <- EXCLUSIVE_MIRS %>% ungroup() %>% filter(SIGN == WHICH_SIGN) %>% distinct(CONTRAST) %>% pull())
 
-print(EXCLUSIVE_UP_MIRS <- do.call(rbind, DF) %>% mutate(SIGN = WHICH_SIGN))
+CONTRAST <- as.character(CONTRAST)
+
+EXCLUSIVE_MIRS %>% unnest(CONTRAST_DE) %>% filter(SIGN == WHICH_SIGN) %>% dplyr::count(SIGN, CONTRAST)
+
+str(query.names <- EXCLUSIVE_MIRS %>% unnest(CONTRAST_DE) %>% 
+    filter(SIGN == WHICH_SIGN & Name != drop.names) %>% distinct(Name) %>% pull())
+
+query.p <- c(rep(0.05, length(query.names)))
+
+EXCLUSIVE_MIRS_ALLRes_110HPF <- GOenrichment(query.p, query.names, SRNA2GO, Nodes = 10)
+
+EXCLUSIVE_MIRS_ALLRes_110HPF <- EXCLUSIVE_MIRS_ALLRes_110HPF %>% mutate(SIGN = WHICH_SIGN, CONTRAST = CONTRAST[1]) %>% as_tibble()
+
+view(rbind(INTERSECTED_MIRS_ALLRes, EXCLUSIVE_MIRS_ALLRes_24HPF, EXCLUSIVE_MIRS_ALLRes_110HPF))
+
+INTERSECTED_MIRS_ALLRes
+
+
+print(EXCLUSIVE_MIRS_ALLRes_110HPF <- do.call(rbind, DF) %>% mutate(SIGN = WHICH_SIGN))
 
 # DOWN
 
@@ -189,7 +289,7 @@ for(j in 1:length(CONTRAST)) {
   i <- j
   
   cat("\nRunning ",CONTRAST[i], "\n")
-
+  
   query.names <- EXCLUSIVE_MIRS %>% ungroup() %>% filter(SIGN == WHICH_SIGN) %>% filter(CONTRAST %in% CONTRAST[i]) %>% pull(Name)
   
   query.p <- c(rep(0.05, length(query.names)))
@@ -292,97 +392,6 @@ TOPGO_EXCLUSIVE %>%
     panel.grid.major.x = element_line(linetype = "dashed", linewidth = 0.5),
     axis.text.y = element_text(angle = 0, size = 5),
     axis.text.x = element_text(angle = 90, vjust = 0.5, size = 10))
-
-
-# INTERSECTED ====
-
-# DOWN INTERSECTED:
-
-str(CONTRAST <- INTERSECTED_MIRS %>% ungroup() %>% filter(SIGN == "Down") %>% distinct(CONTRAST) %>% pull())
-
-DF <- list()
-
-for(j in 1:length(CONTRAST)) {
-  
-  i <- j
-  
-  cat("\nRunning ",CONTRAST[i], "\n")
-  
-  query.names <- INTERSECTED_MIRS %>% ungroup() %>% filter(SIGN == "Down") %>% filter(CONTRAST %in% CONTRAST[i]) %>% pull(Name)
-  
-  query.p <- c(rep(0.05, length(query.names)))
-  
-  allRes <- GOenrichment(query.p, query.names, SRNA2GO, Nodes = 20)
-  
-  allRes <- allRes %>% mutate(CONTRAST = CONTRAST[i]) %>% as_tibble()
-  
-  DF[[i]] <- allRes
-  
-}
-
-INTERSECTED_DOWN_MIRS <- do.call(rbind, DF) %>% mutate(SIGN = "Down")
-
-
-# UP INTERSECTED:
-
-str(CONTRAST <- INTERSECTED_MIRS %>% ungroup() %>% filter(SIGN == "Up") %>% distinct(CONTRAST) %>% pull())
-
-DF <- list()
-
-for(j in 1:length(CONTRAST)) {
-  
-  i <- j
-  
-  cat("\nRunning ",CONTRAST[i], "\n")
-  
-  query.names <- INTERSECTED_MIRS %>% ungroup() %>% filter(SIGN == "Up") %>% filter(CONTRAST %in% CONTRAST[i]) %>% pull(Name)
-  
-  query.p <- c(rep(0.05, length(query.names)))
-  
-  allRes <- GOenrichment(query.p, query.names, SRNA2GO, Nodes = 20)
-  
-  allRes <- allRes %>% mutate(CONTRAST = CONTRAST[i]) %>% as_tibble()
-  
-  DF[[i]] <- allRes
-  
-}
-
-INTERSECTED_UP_MIRS <- do.call(rbind, DF) %>% mutate(SIGN = "Up")
-
-rbind(INTERSECTED_DOWN_MIRS, INTERSECTED_UP_MIRS) -> TOPGO_INTERSECTED
-
-
-
-p <- TOPGO_INTERSECTED %>%
-  # filter(p.adj.ks < 0.05) %>%
-  arrange(Term) %>%
-  mutate(Term = factor(Term, levels= unique(Term))) %>%
-  separate(CONTRAST, into = c("WRAP","CONTRAST"), sep = "[|]") %>%
-  ggplot(aes(y = Term, x = WRAP)) + # , fill = p.adj.ks fill = -log10(p.adj.ks))
-  # geom_col() +
-  # geom_point(aes(size = Annotated)) +
-  geom_tile(aes(fill = Annotated), color = 'white', size = 0.7, width = 1) +
-  geom_text(aes(label = Annotated), color = 'black', family = "GillSans", size = 1.5, angle = 90) +
-  theme_bw(base_family = "GillSans", base_size = 12) +
-  ggh4x::facet_nested( ~ CONTRAST+SIGN, nest_line = F, scales = "free", space = "free") +
-  theme_bw(base_family = "GillSans", base_size = 12) +
-  ggsci::scale_fill_material("blue-grey") +
-  labs(x = "", y = "")
-
-p <- p + theme(legend.position = "none",
-  strip.background = element_rect(fill = 'grey89', color = 'white'),
-  panel.border = element_blank(),
-  plot.title = element_text(hjust = 0),
-  plot.caption = element_text(hjust = 0),
-  panel.grid.minor.y = element_blank(),
-  panel.grid.major.y = element_blank(),
-  panel.grid.minor.x = element_blank(),
-  panel.grid.major.x = element_blank(),
-  # strip.background.y = element_blank(),
-  axis.text.y = element_text(angle = 15, size = 5),
-  axis.text.x = element_text(angle = 90, vjust = 0.5, size = 10))
-
-ggsave(p, filename = 'DESEQ2TOPGO_INTERSECTED.png', path = wd, width = 3.5, height = 10, device = png, dpi = 300)
 
 
 out <- list(TOPGO_EXCLUSIVE, TOPGO_INTERSECTED) 
