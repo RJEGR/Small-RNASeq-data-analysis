61a72aabd58d94d4b91f66f0e22e2d3027052a0e
Ricardo Gore <rjhgore@gmail.com>
2023-10-08 21:09:18 -0700

    Update 6_WGCNA_MIRS.R

--- E_expression_analysis/6_WGCNA_MIRS.R
+++ E_expression_analysis/6_WGCNA_MIRS.R
@@ -31,7 +31,7 @@ DB %>% dplyr::count(Name, sort = T)
 COUNT <- COUNT %>% as_tibble(rownames = "Name") %>%
   right_join(DB %>% distinct(Name, Family), by = "Name") %>%
   group_by(Family) %>%
-  summarise_at(vars(colnames(COUNTS)), sum) 
+  summarise_at(vars(colnames(COUNT)), sum) 
 
 which_cols <- COUNT %>% select_if(is.double) %>% colnames()
 
@@ -71,7 +71,7 @@ par(mfrow=c(1,2))
 hist(k)
 scaleFreePlot(k, main="Check scale free topology\n")
 
-max_power <- 30
+max_power <- 10
 
 powers = c(c(1:10), seq(from = 10, to = max_power, by=1)) 
 
@@ -132,3 +132,279 @@ sft$fitIndices %>%
 
 psave
 
+# Build a adjacency "correlation" matrix
+
+enableWGCNAThreads()
+
+# specify network type
+
+adjacency <- adjacency(datExpr, 
+  power = 5, 
+  type = "unsigned")
+
+
+TOM <- TOMsimilarity(adjacency, TOMType = "signed") # specify network type
+
+
+dissTOM = 1 - TOM
+
+heatmap(TOM)
+
+# Generate Modules ----
+# Generate a clustered gene tree
+
+geneTree = flashClust(as.dist(dissTOM), method="average")
+
+plot(geneTree, xlab="", sub="", 
+  main= "Gene Clustering on TOM-based dissimilarity", labels= FALSE, hang=0.04)
+
+#This sets the minimum number of genes to cluster into a module
+
+minClusterSize <- 3
+
+dynamicMods <- cutreeDynamic(dendro= geneTree, 
+  distM = dissTOM,
+  method = "hybrid",
+  deepSplit = 2, 
+  cutHeight = 0.8,
+  pamRespectsDendro = FALSE,
+  minClusterSize = minClusterSize)
+
+dynamicColors = labels2colors(dynamicMods)
+names(dynamicColors) <- colnames(datExpr)
+MEList = moduleEigengenes(datExpr, 
+  colors= dynamicColors,
+  softPower = softPower)
+
+MEs = MEList$eigengenes
+
+MEDiss= 1 - cor(MEs)
+
+METree = flashClust(as.dist(MEDiss), method= "average")
+
+# Set a threhold for merging modules. In this example we are not merging so MEDissThres=0.0
+
+MEDissThres = 0.0
+
+merge = mergeCloseModules(datExpr, dynamicColors, cutHeight= MEDissThres, verbose =3)
+
+mergedColors = merge$colors
+
+mergedMEs = merge$newMEs
+
+#plot dendrogram with module colors below it
+plotDendroAndColors(geneTree, dynamicColors, c("Modules"), dendroLabels= FALSE, hang=0.03, addGuide= TRUE, guideHang=0.05)
+
+#plotDendroAndColors(geneTree, cbind(dynamicColors, mergedColors), c("Dynamic Tree Cut", "Merged dynamic\n(cutHeight=0.2)"), dendroLabels= FALSE, hang=0.03, addGuide= TRUE, guideHang=0.05)
+
+moduleColors <- mergedColors
+
+colorOrder <- c("grey", standardColors(50))
+
+moduleLabels <- match(moduleColors, colorOrder) - 1
+
+
+MEs <- mergedMEs
+
+
+# how modules where obtained:
+nm <- table(moduleColors)
+
+
+cat('Number of mudules obtained\n :', length(nm))
+
+print(nm)
+
+
+# Plot TOM
+plotTOM = dissTOM^7
+# Set diagonal to NA for a nicer plot
+diag(plotTOM) = NA
+# Call the plot function
+TOMplot(plotTOM, dendro = geneTree, Colors = moduleColors)
+
+#
+
+reads <- colSums(datExpr)
+
+Total <- sum(reads)
+
+# Sanity check:
+
+identical(names(colSums(datExpr)), names(moduleColors))
+
+data.frame(reads, moduleColors) %>% 
+  as_tibble(rownames = "Name") %>% 
+  group_by(moduleColors) %>% 
+  summarise(n = n(), reads = sum(reads)) %>%
+  dplyr::rename('module' = 'moduleColors') -> stats
+
+# Prep binary datTraits
+
+datTraits <- gsub(".clean.newid.subset", "", rownames(datExpr))
+
+HR11076 <- grepl('HR11076', datTraits)
+
+HR1108 <- grepl('HR1108', datTraits)
+
+HR2476 <- grepl('HR2476', datTraits)
+
+HR248 <- grepl('HR248', datTraits)
+
+datTraits <- data.frame(HR11076, HR1108, HR2476, HR248)
+
+datTraits <- 1*datTraits
+
+rownames(datTraits) <- rownames(datExpr)
+
+# Recalculate MEs with color labels
+
+MEs0 = moduleEigengenes(datExpr, moduleColors)$eigengenes
+
+MEs = orderMEs(MEs0)
+
+names(MEs) <- str_replace_all(names(MEs), '^ME', '')
+
+moduleTraitCor = cor(MEs, datTraits, use= "p")
+
+moduleTraitPvalue = corPvalueStudent(moduleTraitCor, nrow(datTraits))
+
+
+moduleTraitCor %>% as_tibble(rownames = 'module') %>% 
+  pivot_longer(-module, values_to = 'moduleTraitCor') -> df1
+
+moduleTraitPvalue %>% as_tibble(rownames = 'module') %>% 
+  pivot_longer(-module, values_to = 'corPvalueStudent') %>%
+  right_join(df1) -> df1
+
+hclust <- hclust(dist(moduleTraitCor), "complete")
+
+df1 %>%
+  mutate(star = ifelse(corPvalueStudent <.001, "***", 
+    ifelse(corPvalueStudent <.01, "**",
+      ifelse(corPvalueStudent <.05, "*", "")))) -> df1
+
+df1 %>%
+  mutate(facet = ifelse(name %in% c('HR11076', 'HR2476'), 'Low pH', 'Control')) %>%
+  mutate(moduleTraitCor = round(moduleTraitCor, 2)) %>%
+  mutate(star = ifelse(star != '', paste0(moduleTraitCor, '(', star,')'), moduleTraitCor)) %>%
+  # mutate(star = ifelse(star != '', paste0(moduleTraitCor, '(', star,')'), '')) %>%
+  ggplot(aes(y = module, x = name, fill = moduleTraitCor)) +
+  geom_tile(color = 'white', size = 0.7, width = 1) +
+  # geom_raster() +
+  geom_text(aes(label = star),  vjust = 0.5, hjust = 0.5, size= 4, family =  "GillSans") +
+  ggsci::scale_fill_gsea(name = "", reverse = T, na.value = "white") +
+  # scale_fill_viridis_c(name = "Membership", na.value = "white") +
+  ggh4x::scale_y_dendrogram(hclust = hclust) +
+  labs(x = '', y = 'Module') +
+  guides(fill = guide_colorbar(barwidth = unit(3.5, "in"),
+    barheight = unit(0.1, "in"), label.position = "top",
+    alignd = 0.5,
+    ticks.colour = "black", ticks.linewidth = 0.5,
+    frame.colour = "black", frame.linewidth = 0.5,
+    label.theme = element_text(size = 10))) +
+  theme_classic(base_size = 12, base_family = "GillSans") +
+  theme(legend.position = "top",
+    strip.background = element_rect(fill = 'white', color = 'white')) +
+  facet_wrap(~ facet, scales = 'free_x') -> p1 
+
+p1 <- p1 + theme(
+  axis.line.x = element_blank(),
+  axis.line.y = element_blank(),
+  axis.text.y = element_text(hjust = 1.2),
+  axis.ticks.length = unit(5, "pt"))
+
+p1 <- p1 + theme(panel.spacing.x = unit(-0.5, "mm"))
+
+# BARPLOT
+
+p2 <- stats %>% 
+  mutate(module = factor(module, levels = hclust$labels[hclust$order])) %>%
+  ggplot(aes(y = module)) +
+  scale_x_continuous("miRs") +
+  geom_col(aes(x = n), width = 0.95, position = position_stack(reverse = TRUE)) +
+  # geom_col(aes(x = reads_frac), width = 0.95, fill = "grey")
+  # scale_fill_manual(name = '', values = c("#303960", "#647687", "#E7DFD5")) + # grey90
+  theme_classic(base_size = 14, base_family = "GillSans") +
+  theme(legend.position = "top",
+    strip.background = element_rect(fill = 'white', color = 'white'),
+    axis.title.y = element_blank(), 
+    axis.text.y= element_blank(),
+    axis.ticks.y =element_blank(), 
+    axis.line.y = element_blank(),
+    axis.line.x = element_blank(),
+    axis.ticks.length = unit(5, "pt"))
+
+
+library(patchwork)
+
+# p1 + plot_spacer() + p2 + plot_layout(widths = c(5,-0.5, 10))  #& theme(plot.margin = 0)
+
+p1 + p2 + plot_layout(widths = c(6, 5)) + labs(caption = '* corPvalueStudent < 0.05 ') 
+
+# network ====
+library(igraph)
+library(tidygraph)
+library(ggraph)
+
+exportNet <- function(TOMobj, moduleColors, threshold = 0.9) {
+  
+  nodeNames <- names(moduleColors)
+  nodeAttr <- moduleColors
+  
+  
+  # file_out <- gsub('.RData', '', basename(TOMFile))
+  
+  file_out <- "WGCNA.mirs"
+    
+  edgeFile <- paste0(path, "/",file_out, ".edges.txt")
+  nodeFile <- paste0(path, "/",file_out, ".nodes.txt")
+  
+  
+  Net = exportNetworkToCytoscape(TOMobj,
+    edgeFile = edgeFile,
+    nodeFile = nodeFile,
+    weighted = TRUE,
+    threshold = threshold,
+    nodeNames = nodeNames, nodeAttr = nodeAttr)
+  
+  cat('\nEdges: ',nrow(Net$edgeData), 'and Nodes: ',nrow(Net$nodeData),'\n')
+  
+  graph <- tidygraph::tbl_graph(nodes = Net$nodeData, edges = Net$edgeData, directed = FALSE)
+  
+  return(graph)
+  
+  # if(is.null(query)) {
+  #   graph
+  # } else {
+  #   graph %>% activate(nodes) %>% filter(nodeName %in% query)
+  # }
+  
+  
+  # return(Net)
+  
+}
+
+g <- exportNet(TOM, moduleColors, threshold = 0)
+
+layout = create_layout(g, layout = 'igraph', algorithm = 'kk')
+
+g %>% 
+  activate("nodes") %>% filter(nodeName %in% "BANTAM") -> g
+
+g %>% activate('nodes') %>% left_join(df %>% select(nodeName, parentTerm)) -> g
+
+
+ggraph(layout) +
+  # facet_nodes(~ module) +
+  # scale_color_manual('', values = structure(psME, names = psME) ) +
+  # geom_edge_arc(aes(edge_alpha = weight), strength = 0.1) + # edge_width
+  geom_node_point(aes(color = `nodeAttr[nodesPresent, ]`, size = degree)) +
+  # geom_node_text(aes(label = parentTerm), repel = TRUE, size = 2) +
+  ggrepel::geom_text_repel(data = layout, aes(x, y, label = nodeName)) +
+  scale_edge_width(range = c(0.3, 1)) +
+  theme_graph(base_family = "GillSans") +
+  guides(fill=guide_legend(nrow = 2)) +
+  theme(legend.position = "top") +
+  coord_fixed() +
+  scale_color_brewer(type = 'qual')
