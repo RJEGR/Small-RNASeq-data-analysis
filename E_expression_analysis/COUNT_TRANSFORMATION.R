d9e1ee6649eeb213a5455fba386eb099a4bbd826
Ricardo Gore <rjhgore@gmail.com>
2023-11-30 16:15:18 -0800

    Update COUNT_TRANSFORMATION.R

--- E_expression_analysis/COUNT_TRANSFORMATION.R
+++ E_expression_analysis/COUNT_TRANSFORMATION.R
@@ -35,7 +35,7 @@ colnames(COUNTS) <- colNames
 
 # OPTIONAL:
 
-# COUNTS <- COUNTS %>% filter(Name %in% query.ids)
+COUNTS <- COUNTS %>% filter(Name %in% query.ids)
 
 # THEN:
 
@@ -63,7 +63,7 @@ nrow(COUNTS <- COUNTS[keep,])
 
 COUNTS <- round(COUNTS)
 
-DESeq2::varianceStabilizingTransformation(COUNTS)
+# DESeq2::varianceStabilizingTransformation(COUNTS)
 
 library(DESeq2)
 
@@ -79,6 +79,8 @@ dds <- estimateDispersions(dds)
 # However for other downstream analyses – e.g. for visualization or clustering – it might be useful to work with transformed versions of the count data. Ex:
 
 vst <- DESeq2::varianceStabilizingTransformation(dds) # vst if cols > 10
+
+# vst <- DESeq2::vst(dds) # vst if cols > 10
 
 ntr <- DESeq2::normTransform(dds)
 
@@ -103,44 +105,110 @@ rbind(data.frame(py = vst_df$sd, px = vst_df$rank, col = "vst"),
 
 # USING VST FOR HEATMAP
 
-DATA <- assay(vst)
+head(DATA <- assay(vst))
 
-heatmap(log2(DATA))
+# heatmap(log2(DATA))
 
 # 
 
 
-sample_cor = cor(assay(vst), method='pearson', use='pairwise.complete.obs')
-sample_dist = dist(t(assay(vst)), method='euclidean')
+sample_cor = cor(DATA, method='pearson', use='pairwise.complete.obs')
+sample_dist = dist(sample_cor, method='euclidean')
 hc_samples = hclust(sample_dist, method='complete')
-
 
 hc_order <- hc_samples$labels[hc_samples$order]
 
-# heatmap(sample_cor, col = cm.colors(12))
+# hc_samples <- as.hclust(reorder(as.dendrogram(hc_samples), 12:1))
+
+h <- heatmap(sample_cor, col = cm.colors(12), keep.dendro = T)
+
+hc_samples <- as.hclust(h$Rowv)
+hc_order <- hc_samples$labels[h$rowInd]
 
 sample_cor %>% 
   as_tibble(rownames = 'LIBRARY_ID') %>%
   pivot_longer(cols = colnames(sample_cor), values_to = 'cor') %>%
   left_join(.colData) %>% 
-  mutate(LIBRARY_ID = factor(LIBRARY_ID, levels = hc_order)) -> sample_cor_long
+  mutate(LIBRARY_ID = factor(LIBRARY_ID, levels = rev(hc_order))) -> sample_cor_long
 
 library(ggh4x)
 
-sample_cor_long %>%
-  ggplot(aes(x = LIBRARY_ID, y = name, fill = cor)) + 
-  geom_tile(color = 'white', linewidth = 0.2) +
-  # geom_raster() + 
-  # geom_text(aes(label = cor), color = 'white') +
-  scale_fill_viridis_c(option = "B", name = "Pearson", direction = -1) +
-  scale_x_discrete(position = 'top') +
-  ggh4x::scale_x_dendrogram(hclust = hc_samples, position = 'top') +
-  ggh4x::scale_y_dendrogram(hclust = hc_samples) +
-  theme_classic(base_size = 12, base_family = "GillSans") +
+P <- sample_cor_long %>%
+  ggplot(aes(x = LIBRARY_ID, y = name, fill = cor)) +  
+  geom_tile(linewidth = 0.2) +
+  ggsci::scale_fill_material(name = "", "blue-grey") +
+  ggsci::scale_color_material(name = "", "blue-grey") +
+  scale_x_discrete(position = 'bottom') +
+  ggh4x::scale_y_dendrogram(hclust = hc_samples, position = "left", labels = NULL) +
+  guides(y.sec = guide_axis_manual(labels = hc_order, label_size = 5, label_family = "GillSans")) +
+  # ggh4x::scale_x_dendrogram(hclust = hc_samples, position = "top", labels = NULL) +
+  theme_bw(base_size = 7, base_family = "GillSans") +
   labs(x = '', y = '') +
-  theme(axis.text.x = element_text(angle = 50,
-    hjust = -0.15, vjust = 1))
+  theme(
+    legend.position = "bottom",
+    # axis.text.x = element_blank(),
+    # axis.ticks.x = element_blank(),
+    axis.text.x = element_text(angle = 90, hjust = 1, size = 5),
+    strip.background = element_rect(fill = 'grey89', color = 'white'),
+    panel.border = element_blank(),
+    plot.title = element_text(hjust = 0),
+    plot.caption = element_text(hjust = 0),
+    panel.grid.minor.y = element_blank(),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.x = element_blank()) 
 
+P <- P + guides(
+  fill = guide_colorbar(barwidth = unit(1.5, "in"),
+    barheight = unit(0.05, "in"), label.position = "bottom",
+    alignd = 0.5,
+    ticks.colour = "black", ticks.linewidth = 0.5,
+    frame.colour = "black", frame.linewidth = 0.5,
+    label.theme = element_text(family = "GillSans", size = 7)))
+
+P
+
+# TOP
+
+recode_to <- c(`24` = "24 hpf", `110` = "110 hpf")
+
+TOPDF <- sample_cor_long %>%
+  distinct(LIBRARY_ID, hpf, pH) %>%
+  dplyr::mutate(hpf = dplyr::recode_factor(hpf, !!!recode_to)) %>%
+  mutate(label = ifelse(pH %in% "Low", "*", "")) %>%
+  mutate(y = 1)
+
+topplot <- TOPDF %>%
+  ggplot(aes(y = y, x = LIBRARY_ID, color = as.factor(hpf))) +
+  geom_point(shape = 15, size = 2) +
+  geom_text(aes(label = label),  vjust = -0.7, hjust = 0.5, size = 1.5, family =  "GillSans", color = "#d73027") +
+  ggh4x::scale_x_dendrogram(hclust = hc_samples, position = 'top', labels = NULL) +
+  # ggh4x::guide_dendro()
+  # guides(x.sec = guide_axis_manual(labels = hc_order, label_size = 3.5)) +
+  theme_bw(base_family = "GillSans", base_size = 10) +
+  # see::scale_color_pizza(name = "", reverse = T) +
+  scale_color_manual("", values = c("#DADADA", "#D4DBC2")) + # "#4575b4", "#d73027"
+  theme(legend.position = 'top',
+    panel.border = element_blank(), 
+    plot.background = element_rect(fill='transparent', color = 'transparent'),
+    plot.margin = unit(c(0,0,0,0), "pt"),
+    panel.grid.minor = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text.y = element_blank(),
+    axis.title = element_blank(),
+    panel.grid.major = element_blank()) 
+
+
+library(patchwork)
+
+psave <- topplot/ plot_spacer() /P + plot_layout(heights = c(0.6, -0.5, 5))
+
+psave
+
+ggsave(psave, filename = 'SAMPLE_HEATMAP_MIR.png', path = path, width = 2.5, height = 3.5, device = png, dpi = 300)
+
+# PCA
+vst <- DESeq2::varianceStabilizingTransformation(dds) # vst if cols > 10
 
 ncol(data <- assay(vst))
 
