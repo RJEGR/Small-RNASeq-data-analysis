59a8ce2d022e1f35d965a986d8bcbcb275e7dd88
Ricardo Gore <rjhgore@gmail.com>
2024-08-05 13:42:24 -0700

    Update GRAPH.R

--- Analysis_for_pub/RELATIONAL_DB/GRAPH.R
+++ Analysis_for_pub/RELATIONAL_DB/GRAPH.R
@@ -1,9 +1,19 @@
 
 library(tidyverse)
+library(igraph)
+library(tidygraph)
+library(ggraph)
 
 rm(list = ls());
 
 if(!is.null(dev.list())) dev.off()
+
+paste_col <- function(x) { 
+  x <- x[!is.na(x)] 
+  x <- unique(sort(x))
+  x <- paste(x, sep = ';', collapse = ';')
+}
+
 
 dir <- "~/Documents/MIRNA_HALIOTIS/MIRNA_PUB_2024/"
 
@@ -11,13 +21,120 @@ QUERYDB <- read_rds(list.files(path = dir,
   pattern = "SEQUENCES_MERGED_DESEQ_RES_WGCNA.rds", full.names = T)) %>%
   filter( padj < 0.05  & abs(log2FoldChange) > 1) %>%
   filter(CONTRAST == "CONTRAST_C") %>%
-  mutate(HPF = ifelse(sign(log2FoldChange) == -1, "24 hpf", "110 hpf"))
+  mutate(HPF = ifelse(sign(log2FoldChange) == -1, "110 hpf", "24 hpf"))
+
+
+QUERYDB <- read_rds(list.files(path = dir, 
+  pattern = "SEQUENCES_MERGED_DESEQ_RES_WGCNA.rds", full.names = T)) %>%
+  filter( padj < 0.05  & abs(log2FoldChange) > 1) %>%
+  filter(CONTRAST == "CONTRAST_A") %>%
+  mutate(HPF = ifelse(sign(log2FoldChange) == -1, "pH 7.6", "pH 8.0"))
+
+QUERYDB %>% count(CONTRAST, HPF)
 
 print(DB <- read_tsv(file.path(dir, "LONGER_RELATIONAL_DB.tsv")))
 
-str(qnodes <- QUERYDB %>% pull(MajorRNA))
+# Module info
 
-Net <- DB %>% filter(MajorRNA %in% qnodes)
+wd <- "/Users/cigom/Documents/MIRNA_HALIOTIS/FUNCTIONAL_MIR_ANNOT/"
+
+
+print(WGCNA <- read_rds(paste0(wd,"SRNA_FUNCTION_PREDICTED_LONG_EXPRESSED.rds")))
+
+WGCNA <- WGCNA %>% distinct(gene_id, WGCNA_target)
+
+# First ----
+
+# STRING
+dir <- "~/Documents/MIRNA_HALIOTIS/ENSEMBLE/calcif/"
+
+stringg <- read_rds(file.path(dir, "protein_links_full_v12.rds"))
+
+
+stringg <- stringg %>% activate("nodes") %>% 
+  mutate(betweenness = betweenness(.), degree = centrality_degree(),
+    pageRank = page_rank(.)$vector) %>%
+  filter(degree > 0)
+
+nodes <- stringg %>% activate("nodes") %>% as_tibble()
+edges <- stringg %>% activate("edges") %>% as_tibble()
+
+DB <- DB %>% mutate(COG_name = ifelse(is.na(COG_name), description, COG_name))
+
+nodes <- DB %>% 
+  right_join(QUERYDB, by = "MajorRNA") %>%
+  # distinct(gene_id, HPF, COG_name) %>%
+  # group_by(gene_id, COG_name) %>%
+  distinct(gene_id, HPF) %>%
+  group_by(gene_id) %>%
+  summarise(across(HPF, .fns = paste_col)) %>%
+  right_join(nodes, by = "gene_id") %>%
+  left_join(WGCNA, by = "gene_id")
+
+nodes <- data.frame(
+  id = as.numeric(factor(nodes$Node)), 
+  label = nodes$preferred_name,
+  group = nodes$HPF,
+  color = nodes$WGCNA_target, # shape
+  value = nodes$degree,
+  nodes)
+
+evidence <- data.frame(edges$fusion > 0,
+edges$neighborhood > 0,
+edges$cooccurence > 0,
+edges$experiments > 0,
+edges$textmining > 0,
+edges$database > 0,
+edges$coexpression > 0)
+
+
+edges <- edges[rowSums(evidence) > 0,]
+evidence <- evidence[rowSums(evidence) > 0,]
+
+edges <- data.frame(
+  value = rowSums(evidence),
+  # label = paste0(rowSums(evidence), " evidences"),
+  edges
+)
+
+# edges$from <- nodes[edges$from,]$Node
+# edges$to <- nodes[edges$to,]$Node
+library(visNetwork)
+
+visNetwork::visNetwork(
+  nodes = nodes,
+  edges = edges, width = "100%") %>%
+  visInteraction(navigationButtons = T) %>%
+  visOptions(highlightNearest = T, nodesIdSelection = T, selectedBy = "COG_name") %>%
+  visLegend(
+    useGroups = FALSE, addNodes = data.frame(label = "Protein", shape = "circle"), 
+    addEdges = data.frame(label = "Predicted association", color = "black"))
+  # visEdges(arrows = 'from')
+
+visNetwork(nodes, edges, width = "100%") %>% 
+  visEdges(arrows = "to") %>% 
+  visHierarchicalLayout(direction = "DU") 
+
+# Second ----
+
+# Third (hard to try)
+# split miRs to viz
+qnodes <- DB %>% right_join(QUERYDB) %>% 
+  distinct(gene_id, MajorRNA) %>%
+  count(gene_id, sort = T) %>% sample_n(2)
+
+q <- qnodes%>% pull(gene_id)
+
+stringg %>% activate("nodes") %>% filter(gene_id %in% q)
+  mutate(Col = ifelse(preferred_name %in% which_targets, "Target under OA", ""),
+    label = ifelse(preferred_name %in% which_targets, preferred_name, ""))
+
+
+
+Net <- DB %>% right_join(QUERYDB) %>% filter(gene_id %in% q)
+
+# str(qnodes <- QUERYDB %>% pull(MajorRNA))
+# Net <- DB %>% filter(MajorRNA %in% qnodes)
 
 Net <- Net %>% distinct(MajorRNA, gene_id) %>%
   dplyr::rename("from" = "MajorRNA", "to" = "gene_id")
@@ -31,26 +148,31 @@ NodeInfo <- DB %>%
   rename("Node" = "gene_id")
 
 NodeInfo2 <- QUERYDB %>% 
-  distinct(MajorRNA, HPF, WGCNA) %>% 
+  distinct(MajorRNA, HPF, WGCNA, MirGeneDB_ID) %>% 
   rename("Node" = "MajorRNA")
 
 # Make it hierarchical
 
 # create a data frame giving the hierarchical structure of your individuals
 
-root <- data.frame(from = "origin", to = unique(Net$from))
+root <- data.frame(from = "root", to = unique(Net$from))
 
-hierarchy <- rbind(root, Net)
+nrow(hierarchy <- rbind(root, Net))
 
 hierarchy <- hierarchy %>% as_tibble()
 
-hierarchy <- hierarchy %>% left_join(NodeInfo2, by = c("from" = "Node"))
+hierarchy <- hierarchy %>% 
+  left_join(NodeInfo2, by = c("to" = "Node"))
+
+# view(hierarchy)
 
 # hierarchy %>% mutate()
 # create a vertices data.frame. One line per object of our hierarchy
 
-Nodes <- data.frame(Node = unique(c(hierarchy$from, hierarchy$to))) %>% 
-  mutate_if(is.character, as.factor) 
+Nodes <- data.frame(Node = unique(c(hierarchy$from, hierarchy$to))) 
+
+Nodes%>% 
+  mutate_if(is.character, as.factor)
 
 Nodes <- Nodes %>% left_join(NodeInfo2) %>% left_join(NodeInfo)
 
@@ -73,28 +195,47 @@ to <- match(Net$to, Nodes$Node)
 graph <- graph %>% activate("nodes") %>%
   mutate(betweenness = betweenness(.), degree = centrality_degree())
 
-# Basic graph
-# https://github.com/thomasp85/ggraph/issues/272
-# https://lemuelkumarga.com/viewer/?key=wildlife-trade
-# https://r-graph-gallery.com/310-custom-hierarchical-edge-bundling.html
-
-
 # Use class inheritance for layout but plot class imports as bundles
-ggraph(graph, 'dendrogram', circular = TRUE) +
-  # geom_conn_bundle(aes(colour = HPF, group = HPF),
+edge_bundle_plot <- ggraph(graph, 'dendrogram', circular = T) +
+  theme_void()
+
+edge_bundle_plot$data["angle"] <- sapply(1:nrow(edge_bundle_plot$data), 
+  function (i) {
+    t_ratio <- min(max(edge_bundle_plot$data$y[i] / edge_bundle_plot$data$x[i], -10000),10000)
+    atan(t_ratio) * 180 / pi
+  })
+
+edge_bundle_plot$data["hjust"] <- sapply(edge_bundle_plot$data$x, 
+  function (x) {
+    ifelse(x < 0, 1, 0)
+  })
+
+edge_bundle_plot <- edge_bundle_plot +
   geom_conn_bundle(aes(colour = after_stat(index)),
     data = get_con(from, to),
     edge_alpha = 0.25,
     width=0.9, 
     tension=1
   ) +
-  geom_node_point(aes(filter = leaf, colour = COG_category)) +
-  scale_edge_colour_distiller('', direction = 1, guide = 'edge_coloursteps') +
-  coord_fixed() +
-  ggforce::theme_no_axes()
+  coord_fixed()
+
+# Create Nodes
+edge_bundle_plot +
+  geom_node_point(aes(filter = leaf, x = x, y=y), alpha=0.8) +
+  geom_node_text(aes(filter = leaf, x = x*1.05, y=y*1.05, 
+    label=Node, # colour=region, alpha=ranking,
+    angle=angle, hjust=hjust), size=2.7) 
+
+
+
+# Basic graph
+# https://github.com/thomasp85/ggraph/issues/272
+# https://lemuelkumarga.com/viewer/?key=wildlife-trade
+# https://r-graph-gallery.com/310-custom-hierarchical-edge-bundling.html
+
+
+
   # facet_nodes(~ HPF)
-
-
 # Continue w/ other netviz
 
 
@@ -124,13 +265,6 @@ Nodes <- Nodes %>% left_join(NodeInfo) %>% left_join(NodeInfo2)
 
 # Edges <- data.frame(from = net$gene_id, to = net$MajorRNA)
 graph <- tidygraph::tbl_graph(nodes = Nodes, edges = Net, directed = T)
-
-# graph <- graph_from_data_frame(Nodes, vertices=Net )
-
-
-library(igraph)
-library(tidygraph)
-library(ggraph)
 
 graph <- graph %>% activate("nodes") %>%
   mutate(betweenness = betweenness(.), degree = centrality_degree())
@@ -163,10 +297,12 @@ ggraph(layout) +
   theme_graph(base_family = "GillSans")
 
 
-ggraph(graph, layout = 'linear', circular = TRUE) + 
-  geom_node_point(aes(color = WGCNA)) 
-
-
 ggraph(graph, 'matrix', sort.by = node_rank_spectral()) + 
   geom_edge_point(mirror = TRUE) + 
+  coord_fixed()
+
+ggraph(graph, 'stress') + 
+  geom_node_voronoi(aes(fill = WGCNA), max.radius = 0.05) + 
+  # geom_node_point() + 
+  geom_edge_link() + 
   coord_fixed()
