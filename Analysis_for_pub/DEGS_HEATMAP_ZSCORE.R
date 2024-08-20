c4ddb698d2c35815c9e011562a962988c85d501d
Ricardo Gore <rjhgore@gmail.com>
2024-08-20 14:35:35 -0700

    Update DEGS_HEATMAP_ZSCORE.R

--- Analysis_for_pub/DEGS_HEATMAP_ZSCORE.R
+++ Analysis_for_pub/DEGS_HEATMAP_ZSCORE.R
@@ -1,28 +1,43 @@
 
 # PLOT HEATMAP OF DEGS, USING Z SCORE
 
+rm(list = ls());
+
+if(!is.null(dev.list())) dev.off()
+
+
 library(DESeq2)
 
+library(tidyverse)
 
-wd <-  "~/Documents/MIRNA_HALIOTIS/MIRNA_PUB_2024"
 
-dds <- read_rds(list.files(path = wd, pattern = "DDS_DESEQ2.rds", full.names = T))
+dir <-  "~/Documents/MIRNA_HALIOTIS/MIRNA_PUB_2024"
+
+
+print(DB <- read_tsv(file.path(dir, "LONGER_RELATIONAL_DB.tsv")))
+
+
+dds <- read_rds(list.files(path = dir, pattern = "DDS_DESEQ2.rds", full.names = T))
 
 colData <- data.frame(colData(dds))
 
 WHICH_CONTRAST <- c("CONTRAST_A", "CONTRAST_B")
 
-RES.P <- read_tsv(list.files(path = wd, pattern = "DESEQ_RES.tsv", full.names = T)) %>% 
+RES <- read_rds(paste0(dir, "/SEQUENCES_MERGED_DESEQ_RES_WGCNA.rds")) %>%
+  mutate(Contrast = ifelse(sign(log2FoldChange) == -1, sampleB, sampleA)) %>%
+  mutate(Contrast = paste0(CONTRAST,"_",Contrast))
+
+RES.P <- RES %>% 
   filter( padj < 0.05  & abs(log2FoldChange) > 1) %>%
   filter(CONTRAST %in% WHICH_CONTRAST)
-  
-  # mutate(log2FoldChange = log2FoldChange * -1)
 
 which_mirs <- RES.P %>% 
   # filter(CONTRAST_DE %in% WHICH_CONTRAST) %>%
   distinct(MajorRNA) %>% pull()
 
 str(which_mirs)  
+
+
 
 # divide the counts by the size factors or normalization factors before
 barplot(cnts <- DESeq2::counts(dds, normalized = F, replaced = F)[which_mirs,])
@@ -78,7 +93,24 @@ identical(order_mirs, names(labels))
 
 order_mirs <- labels
 
-# order_mirs <- paste0(hc_order, " (", module_size, ")")
+COG_names <- DB %>%
+  filter(MajorRNA %in% names(order_mirs)) %>%
+  drop_na(COG_name) %>% 
+  dplyr::count(MajorRNA, COG_name, sort = T) %>%
+  group_by(MajorRNA) %>% slice_head(n = 1) %>%
+  summarise(across(COG_name, .fns = paste_col)) %>%
+  pull(COG_name, name = MajorRNA)
+  
+
+# identical(names(COG_names), names(order_mirs)) # FALSE
+
+COG_names <- COG_names[match(names(order_mirs), names(COG_names))]
+
+identical(names(COG_names), names(order_mirs)) # TRUE
+
+order_mirs <- structure(paste0(order_mirs, " (", COG_names, ")"), names = names(order_mirs))
+
+# order_mirs <- paste0(order_mirs, " (", COG_names, ")")
 
 
 lo = floor(min(DATA$fill))
@@ -90,6 +122,7 @@ library(ggh4x)
 DATA %>%
   dplyr::mutate(pH = dplyr::recode_factor(pH, !!!c(`Control` = "pH 8.0", `Low` = "pH 7.6"))) %>%
   dplyr::mutate(hpf = dplyr::recode_factor(hpf, !!!c(`24` = "24 hpf", `110` = "110 hpf"))) %>%
+  mutate(LIBRARY_ID = gsub("HR", "", LIBRARY_ID)) %>%
   ggplot(aes(x = LIBRARY_ID, y = MajorRNA, fill = fill)) +  
   geom_tile(color = 'white', linewidth = 0.7, width = 1) +
   # facet_grid(~ hpf+pH, scales = "free_x") +
@@ -98,15 +131,14 @@ DATA %>%
     name = NULL) +
   ggh4x::scale_y_dendrogram(hclust = hc_mirs, position = "left", labels = NULL) +
   guides(y.sec = guide_axis_manual(labels = order_mirs, label_size = 8, label_family = "GillSans")) +
-  # scale_y_discrete(labels = order_mirs)
-  theme_bw(base_size = 8, base_family = "GillSans") +
+  theme_bw(base_size = 10, base_family = "GillSans") +
   labs(x = '', y = '') +
   theme(
     legend.position = "top",
     # axis.text.x = element_blank(),
     # axis.ticks.x = element_blank(),
     axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5, size = 7.5),
-    strip.background = element_rect(fill = 'grey89', color = 'white'),
+    # strip.background = element_rect(fill = 'grey89', color = 'white'),
     panel.border = element_blank(),
     plot.title = element_text(hjust = 0),
     plot.caption = element_text(hjust = 0),
@@ -115,30 +147,40 @@ DATA %>%
     panel.grid.minor.x = element_blank(),
     panel.grid.major.x = element_blank()) -> P
 
-P <- P + ggh4x::facet_nested(~ hpf+pH, nest_line = T, scales = "free_x", space = "free_x") +
-  theme(strip.background = element_rect(fill = 'white', color = 'white')) 
+P <- P + 
+  ggh4x::facet_nested(~ hpf+pH, nest_line = T, scales = "free_x", space = "free_x") +
+  theme(
+    strip.text.x = element_text(angle = 0, hjust = 1),
+    strip.placement = "outside",
+    strip.background = element_rect(fill = 'white', color = 'white')) 
 
 P <- P + guides(
-  fill = guide_colorbar(barwidth = unit(3, "in"),
-    barheight = unit(0.1, "in"), label.position = "top",
-    alignd = 0.5,
-    ticks.colour = "black", ticks.linewidth = 0.5,
-    frame.colour = "black", frame.linewidth = 0.5,
-    label.theme = element_text(size = 10, family = "GillSans")))
+  fill = guide_colorbar(barwidth = unit(2.5, "in"),
+    barheight = unit(0.07, "in"), 
+    label.position = "top",
+    title = "Z-score",
+    title.position  = "top",
+    title.theme = element_text(size = 7, family = "GillSans", hjust = 1),
+    alignd = 0.8,
+    ticks.colour = "black", ticks.linewidth = 0.25,
+    frame.colour = "black", frame.linewidth = 0.25,
+    label.theme = element_text(size = 7, family = "GillSans")))
 
-P
+
 
 recode_to <- c(`24` = "24 hpf", `110` = "110 hpf")
 
 TOPDF <- DATA %>%
   distinct(LIBRARY_ID, hpf, pH) %>%
   dplyr::mutate(hpf = dplyr::recode_factor(hpf, !!!recode_to)) %>%
-  mutate(label = ifelse(pH %in% "Low", "*", "")) %>%
+  # mutate(label = ifelse(pH %in% "Low", "*", "")) %>%
+  # mutate(shape = ifelse(pH == "Low", "pH 7.6", "")) %>%
   mutate(y = 1)
 
 topplot <- TOPDF %>%
   ggplot(aes(y = y, x = LIBRARY_ID)) + # color = as.factor(hpf)
   # geom_point(shape = 15, size = 2) +
+  # geom_point(aes(shape = shape, color = shape), alpha = 0.5) +
   # geom_text(aes(label = label),  vjust = -0.7, hjust = 0.5, size = 1.5, family =  "GillSans", color = "#d73027") +
   ggh4x::scale_x_dendrogram(hclust = hc_samples, position = 'bottom', labels = NULL) +
   # ggh4x::guide_dendro()
@@ -159,8 +201,48 @@ topplot <- TOPDF %>%
 
 library(patchwork)
 
-psave <- P/ plot_spacer() /topplot + plot_layout(heights = c(4, -0.5, 0.1))
+psave <- P/ plot_spacer() /topplot + plot_layout(heights = c(3, -0.6, 0.1))
 
 # psave
 
-ggsave(psave, filename = 'DESEQ2_DEGS_HEATMAP_MIR.png', path = wd, width = 4.5, height = 5, device = png, dpi = 300)
+ggsave(psave, filename = 'DESEQ2_DEGS_HEATMAP_MIR.png', path = dir, 
+  width = 6.5, height = 4.2, device = png, dpi = 300)
+
+# Merge w/ COGs ------
+paste_col <- function(x) { 
+  x <- x[!is.na(x)] 
+  x <- unique(sort(x))
+  x <- paste(x, sep = ';', collapse = ';') 
+  
+  return(x)
+  
+}
+
+dir <- "~/Documents/MIRNA_HALIOTIS/MIRNA_PUB_2024/"
+
+print(DB <- read_tsv(file.path(dir, "LONGER_RELATIONAL_DB.tsv")))
+
+
+DB %>%
+  filter(MajorRNA %in% which_mirs) %>%
+  drop_na(COG_name) %>% 
+  group_by(MajorRNA) %>%
+  summarise(across(COG_category, .fns = paste_col), n = n()) %>%
+  left_join(DATA) %>%
+  dplyr::mutate(pH = dplyr::recode_factor(pH, !!!c(`Control` = "pH 8.0", `Low` = "pH 7.6"))) %>%
+  dplyr::mutate(hpf = dplyr::recode_factor(hpf, !!!c(`24` = "24 hpf", `110` = "110 hpf"))) %>%
+  ggplot(aes(x = LIBRARY_ID, y = MajorRNA, fill = fill)) +  
+  geom_tile(color = 'white', linewidth = 0.7, width = 1) +
+  # facet_grid(~ hpf+pH, scales = "free_x") +
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+    na.value = "white", midpoint = mid, limit = c(lo, up),
+    name = NULL) +
+  ggh4x::scale_y_dendrogram(hclust = hc_mirs, position = "left", labels = NULL) +
+  guides(y.sec = guide_axis_manual(labels = order_mirs, label_size = 8, label_family = "GillSans")) +
+  theme_bw(base_size = 8, base_family = "GillSans") +
+  ggh4x::facet_nested(~ hpf+pH, nest_line = T, scales = "free_x", space = "free_x") +
+  theme(strip.background = element_rect(fill = 'white', color = 'white'))
+  
+  
+DATA %>% 
+  left_join(DB)
