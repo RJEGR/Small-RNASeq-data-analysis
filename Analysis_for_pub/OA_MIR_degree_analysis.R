2b803eab5356d628ff29ee2238b7a326eef66c50
Ricardo Gore <rjhgore@gmail.com>
2025-01-22 13:33:39 -0800

    Update OA_MIR_degree_analysis.R

--- Analysis_for_pub/OA_MIR_degree_analysis.R
+++ Analysis_for_pub/OA_MIR_degree_analysis.R
@@ -64,6 +64,7 @@ NodeDF <- DB %>%
   right_join(NodeDF, by = "gene_id")
 
 print(BT <- read_tsv(file.path(dir, "Supplementary_tables - Biological_themes.tsv"), col_names = T))
+
 BT <- BT %>% select(-microRNA_degree, -STRINGID)
 
 NodeDF <- NodeDF %>%
@@ -109,6 +110,8 @@ DataViz %>%
     panel.grid.major = element_blank()) -> P
 
 
+
+
 dat_text <- DataViz %>% 
   # select(Label) %>%
   mutate(Text = gsub("__.+$", "", Label), 
@@ -128,12 +131,14 @@ ggsave(P, filename = 'Mirdegree_COGS.png', path = dir, width = 4, height = 10, d
 
 
 DataVizsum <- DataViz %>%
+  mutate(COG_name = as.character(COG_name)) %>%
   group_by(Contrast, COG_name) %>%
   summarise(OA_MIR_degree = sum(OA_MIR_degree), MIR_degree = sum(MIR_degree)) %>%
+  group_by(Contrast) %>%
   arrange(desc(OA_MIR_degree), .by_group = T) %>% 
-  mutate(Label = as.character(COG_name), row_number = min_rank(Label)) %>% 
-  mutate(Label = factor(paste(Label, row_number, sep = "__"), 
-    levels = rev(paste(Label, row_number, sep = "__")))) 
+  mutate(Label = as.character(COG_name), row_number = row_number(Label)) %>% 
+  # mutate(Label = paste(Label, row_number, sep = "__")) %>%
+  mutate(Label = factor(paste(Label, row_number, sep = "__"), levels = rev(paste(Label, row_number, sep = "__")))) 
 
 
 DataVizsum %>%
@@ -160,23 +165,34 @@ DataVizsum %>%
     panel.grid.major = element_blank()) -> P
 
 
+
 P
 
 ggsave(P, filename = 'Mirdegree_sum_COGS.png', path = dir, width = 4.5, height = 4, device = png, dpi = 500)
 
 # fold-increase decrease 
 
-
 RESviz <- RES.p %>% distinct(MajorRNA, MirGeneDB_ID, log2FoldChange) %>%
+  # mutate(Contrast = ifelse(MirGeneDB_ID == "MIR-133-3p" & log2FoldChange > 0, NA, "")) %>% drop_na(Contrast) %>%
+  filter(log2FoldChange < 0) %>%
   mutate(Contrast = ifelse(MirGeneDB_ID %in% names(QUERIES[1:3]), "24 hpf", "110 hpf")) %>%
   mutate(Contrast = factor(Contrast, levels = c("24 hpf", "110 hpf")))
 
-levelshpf <- DataVizsum %>% ungroup %>% 
-  # filter(Contrast %in% "24 hpf") %>% 
-  distinct(COG_name, Contrast) %>%
-  mutate(COG_name = factor(paste(COG_name, Contrast, sep = "__"))) %>% pull(COG_name)
-
 # Lev110hpf <- DataVizsum %>% ungroup %>% filter(Contrast %in% "110 hpf") %>% distinct(COG_name) %>% pull()
+
+
+DB %>% 
+  filter(MajorRNA %in% QUERIES) %>%
+  mutate(COG_name = paste0(COG_category, ", ",COG_name)) %>%
+  filter(!COG_name %in% "NA, NA") %>%
+  distinct(MajorRNA, COG_name) %>%
+  # left_join(DataVizsum %>% ungroup() %>% distinct(COG_name, row_number))
+  right_join(RESviz, by = "MajorRNA")
+
+DF1 <- DataVizsum %>% 
+  mutate(names_to = "OA_MIR_degree") %>%
+  dplyr::rename("value_to" = "OA_MIR_degree") %>%
+  select(Contrast, Label, names_to, value_to)
 
 DB %>% 
   filter(MajorRNA %in% QUERIES) %>%
@@ -185,15 +201,36 @@ DB %>%
   distinct(MajorRNA, COG_name) %>%
   # left_join(DataVizsum %>% ungroup() %>% distinct(COG_name, row_number))
   right_join(RESviz, by = "MajorRNA") %>%
-  mutate(COG_name = paste(COG_name, Contrast, sep = "__")) %>%
-  filter(COG_name %in% as.character(levelshpf)) %>%
-  mutate(COG_name = factor(COG_name, levels = rev(as.character(levelshpf)))) %>%
-  ggplot(aes(y = COG_name, x = log2FoldChange)) +
+  mutate(Label = paste(COG_name, Contrast, sep = "__")) %>%
+  mutate(names_to = "log2FoldChange") %>%
+  dplyr::rename("value_to" = "log2FoldChange") %>%
+  select(Contrast, Label, names_to, value_to) %>%
+  rbind(DF1) %>%
+  ggplot(aes(y = Label, x = value_to)) +
+  geom_segment(aes(xend = 0, yend = Label), linewidth = 1.3) +
+  # scale_y_discrete(labels = function(x) gsub("__.+$", "", x)) +
+  # geom_jitter(alpha = 0.5) +
+  # stat_boxplot(geom ='errorbar', width = 0.3, linetype="dashed") +
+  geom_point(shape = 21, size = 1, stroke = 1.2, fill = "white", alpha = 0.5) +
+  facet_grid( Contrast ~ names_to, space = "free", scales = "free") 
+
+
+DB %>% 
+  filter(MajorRNA %in% QUERIES) %>%
+  mutate(COG_name = paste0(COG_category, ", ",COG_name)) %>%
+  filter(!COG_name %in% "NA, NA") %>%
+  distinct(MajorRNA, COG_name) %>%
+  # left_join(DataVizsum %>% ungroup() %>% distinct(COG_name, row_number))
+  right_join(RESviz, by = "MajorRNA") %>%
+  mutate(Label = paste(COG_name, Contrast, sep = "__")) %>%
+  # filter(COG_name %in% as.character(levelshpf)) %>%
+  mutate(Label = factor(Label, levels = rev(levelshpf))) %>%
+  ggplot(aes(y = Label, x = log2FoldChange)) +
   scale_y_discrete(labels = function(x) gsub("__.+$", "", x)) +
   # geom_jitter(alpha = 0.5) +
+  stat_boxplot(geom ='errorbar', width = 0.3, linetype="dashed") +
   geom_point(shape = 21, size = 1, stroke = 1.2, fill = "white", alpha = 0.5) +
-  # stat_boxplot(geom ='errorbar', width = 0.3, linetype="dashed") +
-  # geom_segment(aes(xend = 3, yend = COG_name), linewidth = 0.5, color="#E7DFD5", linetype="dashed") +
+  # geom_segment(aes(xend = 0, yend = COG_name), linewidth = 0.5, color="#E7DFD5") +
   # geom_boxplot(width = 0.3, outlier.alpha = 0, linetype="dashed") +
   ggforce::facet_col(~ Contrast, space = "free", scales = "free_y") +
   theme_bw(base_family = "GillSans", base_size = 10) +
@@ -211,6 +248,30 @@ DB %>%
     # axis.title = element_blank(),
     panel.grid.major = element_blank()) +
   geom_vline(xintercept = 0, linetype = "dashed", size = 0.5) -> P2
+
+# P2 +  geom_text(data = RESviz, 
+#   aes(label=MirGeneDB_ID), hjust= -0.25, vjust = 0, size = 1.5, family = "GillSans", color = "gray20") 
+# 
+
+# DB %>% 
+#   filter(MajorRNA %in% QUERIES) %>%
+#   mutate(COG_name = paste0(COG_category, ", ",COG_name)) %>%
+#   filter(!COG_name %in% "NA, NA") %>%
+#   distinct(MajorRNA, COG_name) %>%
+#   # left_join(DataVizsum %>% ungroup() %>% distinct(COG_name, row_number))
+#   right_join(RESviz, by = "MajorRNA") %>%
+#   mutate(COG_name = paste(COG_name, Contrast, sep = "__")) %>%
+#   filter(COG_name %in% as.character(levelshpf)) %>%
+#   mutate(COG_name = factor(COG_name, levels = rev(as.character(levelshpf)))) %>%
+#   ggplot(aes(y = COG_name, x = log2FoldChange)) +
+#   # ggforce::facet_col(~ Contrast, space = "free", scales = "free_y") +
+#   ggridges::geom_density_ridges_gradient(
+#   jittered_points = T,
+#   position = ggridges::position_points_jitter(width = 0.05, height = 0),
+#   point_shape = '|', point_size = 3, point_alpha = 1, alpha = 0.7) 
+#   # scale_x_continuous(breaks = seq(0, 1, by = 0.2)) +
+# 
+
 
 library(patchwork)
 
